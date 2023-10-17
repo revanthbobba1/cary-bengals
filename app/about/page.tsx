b3b9dc0df6dda23da1cb7766b1c33bf65557cc62
AKOD12 <ankith.kodali@gmail.com>
2023-10-17 01:47:23 -0400

    💬 added in league members with misc images

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -7,14 +7,23 @@ import { genPageMetadata } from 'app/seo'
 export const metadata = genPageMetadata({ title: 'League Members' })
 
 export default function Page() {
-  const author = allAuthors.find((p) => p.slug === 'default') as Authors
-  const mainContent = coreContent(author)
-
   return (
     <>
-      <AuthorLayout content={mainContent}>
-        <MDXLayoutRenderer code={author.body.code} />
-      </AuthorLayout>
+      <div className="space-y-2 pb-8 pt-6 md:space-y-5">
+        <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
+          League Members
+        </h1>
+      </div>
+      {allAuthors.map((author) => {
+        const mainContent = coreContent(author as Authors)
+        return (
+          <div key={author.slug}>
+            <AuthorLayout content={mainContent}>
+              <MDXLayoutRenderer code={author.body.code} />
+            </AuthorLayout>
+          </div>
+        )
+      })}
     </>
   )
 }
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -3,7 +3,6 @@ const siteMetadata = {
   title: 'Cary Bengals Fantasy Football',
   author: 'Cary Bengals LLC',
   headerTitle: 'Cary Bengals Fantasy Football',
-  description: 'Home of all Cary Bengals Content',
   language: 'en-us',
   theme: 'system', // system, dark or light
   siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
