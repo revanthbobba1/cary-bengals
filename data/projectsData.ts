6facb786b4d2ec2d8441d9aa2a6687697b982206
AKOD12 <ankith.kodali@gmail.com>
2023-10-17 00:58:58 -0400

    💬 Add in current press conferences

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,32 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Cary Bengals Week 4 Press Conference',
+    description: `Cary Bengals week four press conference featuring Team ETENRAL PAIN's GM Amogh Natu, 
+    Certified Boys Lover GM Joseph Hong, Great Waller China's GM Alvin Bao, 
+    Hammy Heroes GM Sparsh Patel, and a spedcial interview from LA Akers GM Rishi Nookala.`,
+    imgSrc: '/static/images/week-four-press.png',
+    href: 'https://youtu.be/RjeOb-S3xX0?si=_71DY-NszkRVShya',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Cary Bengals Week 3 Press Conference',
+    description: `Cary Bengals week three press conference featuring Team Drake and Travis GM Keshav Sridhar, 
+    Olave Garden GM Revanth Bobba, Chubbin and Grubbin' GM Kirk Broadbelt, 
+    Meet The Stevenson's GM TJ Nanugonda, and LA Akers GM Rishi Nookala.`,
+    imgSrc: '/static/images/week-three-press.png',
+    href: 'https://youtu.be/Y6mEg2kepBw?si=Km5ro6Uo4wU2i6ZZ',
+  },
+  {
+    title: 'Cary Bengals Week 2 Press Conference',
+    description: `Cary Bengals week two press conference featuring the trophy presentation for Cookin' It Up GM Sparsh Patel and 
+    interviews from Team ETERNAL PAIN GM Amogh Natu, Team Drako GM Carter Bilbro, and Disney Pixar Baddies GM Sam Nangali.`,
+    imgSrc: '/static/images/mickey.png',
+    href: 'https://youtu.be/eh3mAfQpIJc?si=bh7LrEL95pzPb3z4',
+  },
+  {
+    title: 'Cary Bengals Week 1 Press Conference',
+    description: `Cary Bengals week two press conference featuring Meet the Stevenson's GM TJ Nanugonda, Code Monkey's GM Ankith Kodali, and Disney Pixar Baddies GM Sam Nangali.`,
+    imgSrc: '/static/images/more_mickey.png',
+    href: 'https://youtu.be/g2sH8AYwwZw?si=1OHZB9q4jZhwwM2q',
   },
 ]
 
