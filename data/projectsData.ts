8ccf259c0505b553a5617d2c2c40c33ccf7ab03f
revanthbobba1@gmail.com <rsbobba@ncsu.edu>
2024-09-18 23:12:59 -0400

    Fixing routes for all players

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -4,7 +4,7 @@ const projectsData = [
     description: `Cary Bengals week four press conference featuring Team ETENRAL PAIN's GM Amogh Natu, 
     Certified Boys Lover GM Joseph Hong, Great Waller China's GM Alvin Bao, 
     Hammy Heroes GM Sparsh Patel, and a spedcial interview from LA Akers GM Rishi Nookala.`,
-    imgSrc: '/static/images/week-four-press.png',
+    imgSrc: '/static/images/thumbnails/week-four-press.png',
     href: 'https://youtu.be/RjeOb-S3xX0?si=_71DY-NszkRVShya',
   },
   {
@@ -12,20 +12,20 @@ const projectsData = [
     description: `Cary Bengals week three press conference featuring Team Drake and Travis GM Keshav Sridhar, 
     Olave Garden GM Revanth Bobba, Chubbin and Grubbin' GM Kirk Broadbelt, 
     Meet The Stevenson's GM TJ Nanugonda, and LA Akers GM Rishi Nookala.`,
-    imgSrc: '/static/images/week-three-press.png',
+    imgSrc: '/static/images/thumbnails/week-three-press.png',
     href: 'https://youtu.be/Y6mEg2kepBw?si=Km5ro6Uo4wU2i6ZZ',
   },
   {
     title: 'Cary Bengals Week 2 Press Conference',
     description: `Cary Bengals week two press conference featuring the trophy presentation for Cookin' It Up GM Sparsh Patel and 
     interviews from Team ETERNAL PAIN GM Amogh Natu, Team Drako GM Carter Bilbro, and Disney Pixar Baddies GM Sam Nangali.`,
-    imgSrc: '/static/images/mickey.png',
+    imgSrc: '/static/images/players/mickey.png',
     href: 'https://youtu.be/eh3mAfQpIJc?si=bh7LrEL95pzPb3z4',
   },
   {
     title: 'Cary Bengals Week 1 Press Conference',
     description: `Cary Bengals week two press conference featuring Meet the Stevenson's GM TJ Nanugonda, Code Monkey's GM Ankith Kodali, and Disney Pixar Baddies GM Sam Nangali.`,
-    imgSrc: '/static/images/more_mickey.png',
+    imgSrc: '/static/images/players/more_mickey.png',
     href: 'https://youtu.be/g2sH8AYwwZw?si=1OHZB9q4jZhwwM2q',
   },
 ]
