fe2214872e3ecd7a431e0c9caac8eb2b1eb3e061
revanthbobba1@gmail.com <rsbobba@ncsu.edu>
2025-01-11 16:32:37 -0500

    Updating routes to point to newsfeed

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,6 +1,6 @@
 const headerNavLinks = [
   { href: '/', title: 'Home' },
-  { href: '/newsfeed', title: 'Previews & Recaps' },
+  { href: '/newsfeed/page/1', title: 'Previews & Recaps' },
   { href: '/tags', title: 'Power Rankings' },
   { href: '/conferences', title: 'Press Conferences' },
   { href: '/league-members', title: 'League Members' },
