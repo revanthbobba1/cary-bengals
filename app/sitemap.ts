2748fe9fa6f5f4dba4af7fcf8691bc62ea950a55
revanthbobba1@gmail.com <rsbobba@ncsu.edu>
2025-09-30 23:18:14 -0400

    cleanup

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,7 +9,7 @@ export default function sitemap(): MetadataRoute.Sitemap {
     lastModified: post.lastmod || post.date,
   }))
 
-  const routes = ['', 'newsfeed', 'projects'].map((route) => ({
+  const routes = ['', 'newsfeed', 'projects', 'conferences'].map((route) => ({
     url: `${siteUrl}/${route}`,
     lastModified: new Date().toISOString().split('T')[0],
   }))
