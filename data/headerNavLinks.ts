0b8f069e6333e58a1454192753cafc41ae7ea1a2
revanthbobba1@gmail.com <rsbobba@ncsu.edu>
2025-10-01 00:15:28 -0400

    Adding commissioners poll

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,7 +3,7 @@ const headerNavLinks = [
   { href: '/newsfeed/page/1', title: 'Previews & Recaps' },
   { href: '/conferences', title: 'Press Conferences' },
   { href: '/league-members', title: 'League Members' },
-  { href: '/poll', title: 'Commisioner’s Poll'},
+  { href: '/poll', title: 'Commissioner’s Poll'},
 ]
 
 export default headerNavLinks
